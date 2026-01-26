0550aeea1c9a75affada4f73c8b55693c4137764
Benjamin Walkenhorst <krylon@posteo.de>
2026-01-26 16:29:37 +0100

    web - copy-paste static files from prior project

---
+++ web/web.go
@@ -0,0 +1,8 @@
+// /home/krylon/go/src/github.com/blicero/guangng/web/web.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 26. 01. 2026 by Benjamin Walkenhorst
+// (c) 2026 Benjamin Walkenhorst
+// Time-stamp: <2026-01-26 16:27:17 krylon>
+
+// Package web provides a web-based UI.
+package web
