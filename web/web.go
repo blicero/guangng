9d6a4013d1c8d38650298206b159209d80095444
Benjamin Walkenhorst <krylon@posteo.de>
2026-01-26 17:56:41 +0100

    web - start doodling around

--- web/web.go
+++ web/web.go
@@ -2,36 +2,274 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 26. 01. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-01-26 16:32:29 krylon>
+// Time-stamp: <2026-01-26 17:55:22 krylon>
 
 // Package web provides a web-based UI.
 package web
 
 import (
 	"embed"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
 	"log"
+	"net/http"
+	"path/filepath"
+	"regexp"
 	"sync"
 	"sync/atomic"
 	"text/template"
 
+	"github.com/blicero/guangng/common"
 	"github.com/blicero/guangng/database"
+	"github.com/blicero/guangng/logdomain"
+	"github.com/blicero/guangng/nexus"
 	"github.com/gorilla/mux"
 )
 
 const (
 	cacheControl = "max-age=3600, public"
 	noCache      = "no-store, max-age=0"
+	tmplFolder   = "assets/templates"
 )
 
 //go:embed assets
 var assets embed.FS
 
+// Server provides a web-based UI
 type Server struct {
-	addr   string
-	log    *log.Logger
-	pool   *database.Pool
-	lock   sync.RWMutex
-	active atomic.Bool
-	router *mux.Router
-	tmpl   *template.Template
+	addr      string
+	log       *log.Logger
+	pool      *database.Pool // nolint: unused
+	lock      sync.RWMutex   // nolint: unused
+	active    atomic.Bool
+	router    *mux.Router
+	tmpl      *template.Template
+	web       http.Server
+	mimeTypes map[string]string
+	nx        *nexus.Nexus // nolint: unused
 }
+
+// Create returns a new web Server.
+func Create(addr string, nx *nexus.Nexus) (*Server, error) {
+	var (
+		err error
+		msg string
+		srv = &Server{
+			addr: addr,
+			mimeTypes: map[string]string{
+				".css":  "text/css",
+				".map":  "application/json",
+				".js":   "text/javascript",
+				".png":  "image/png",
+				".jpg":  "image/jpeg",
+				".jpeg": "image/jpeg",
+				".webp": "image/webp",
+				".gif":  "image/gif",
+				".json": "application/json",
+				".html": "text/html",
+			},
+		}
+	)
+
+	if srv.log, err = common.GetLogger(logdomain.Web); err != nil {
+		return nil, err
+	}
+
+	// ...
+	var templates []fs.DirEntry
+	var tmplRe = regexp.MustCompile("[.]tmpl$")
+
+	if templates, err = assets.ReadDir(tmplFolder); err != nil {
+		srv.log.Printf("[ERROR] Cannot read embedded templates: %s\n",
+			err.Error())
+		return nil, err
+	}
+
+	srv.tmpl = template.New("").Funcs(funcmap)
+	for _, entry := range templates {
+		var (
+			content []byte
+			path    = filepath.Join(tmplFolder, entry.Name())
+		)
+
+		if !tmplRe.MatchString(entry.Name()) {
+			continue
+		} else if content, err = assets.ReadFile(path); err != nil {
+			msg = fmt.Sprintf("Cannot read embedded file %s: %s",
+				path,
+				err.Error())
+			srv.log.Printf("[CRITICAL] %s\n", msg)
+			return nil, errors.New(msg)
+		} else if srv.tmpl, err = srv.tmpl.Parse(string(content)); err != nil {
+			msg = fmt.Sprintf("Could not parse template %s: %s",
+				entry.Name(),
+				err.Error())
+			srv.log.Println("[CRITICAL] " + msg)
+			return nil, errors.New(msg)
+		} else if common.Debug {
+			srv.log.Printf("[TRACE] Template \"%s\" was parsed successfully.\n",
+				entry.Name())
+		}
+	}
+
+	srv.router = mux.NewRouter()
+	srv.web.Addr = addr
+	srv.web.ErrorLog = srv.log
+	srv.web.Handler = srv.router
+
+	// Register URL handlers
+	srv.router.HandleFunc("/favicon.ico", srv.handleFavIco)
+	srv.router.HandleFunc("/static/{file}", srv.handleStaticFile)
+
+	return srv, nil
+} // func Create(addr string, nx *nexus.Nexus) (*Server, error)
+
+// IsActive returns the Server's active flag.
+func (srv *Server) IsActive() bool {
+	return srv.active.Load()
+} // func (srv *Server) IsActive() bool
+
+// Stop clears the Server's active flag.
+func (srv *Server) Stop() {
+	srv.active.Store(false)
+} // func (srv *Server) Stop()
+
+//////////////////////////////////////////////////////////////////////////////
+/// Handle requests //////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+func (srv *Server) handleMain(w http.ResponseWriter, req *http.Request) {
+	srv.log.Printf("[TRACE] Handle request for %s\n",
+		req.URL.EscapedPath())
+
+	const tmplName = "main"
+
+	var (
+		err  error
+		msg  string
+		db   *database.Database
+		tmpl *template.Template
+		data = tmplDataIndex{
+			tmplDataBase: tmplDataBase{
+				Title: "Main",
+				Debug: common.Debug,
+				URL:   req.URL.String(),
+			},
+		}
+	)
+} // func (srv *Server) handleMain(w http.ResponseWriter, req *http.Request)
+
+//////////////////////////////////////////////////////////////////////////////
+/// Handle static assets /////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+func (srv *Server) handleFavIco(w http.ResponseWriter, request *http.Request) {
+	srv.log.Printf("[TRACE] Handle request for %s\n",
+		request.URL.EscapedPath())
+
+	const (
+		filename = "assets/static/favicon.ico"
+		mimeType = "image/vnd.microsoft.icon"
+	)
+
+	w.Header().Set("Content-Type", mimeType)
+
+	if !common.Debug {
+		w.Header().Set("Cache-Control", "max-age=7200")
+	} else {
+		w.Header().Set("Cache-Control", "no-store, max-age=0")
+	}
+
+	var (
+		err error
+		fh  fs.File
+	)
+
+	if fh, err = assets.Open(filename); err != nil {
+		msg := fmt.Sprintf("ERROR - cannot find file %s", filename)
+		srv.sendErrorMessage(w, msg)
+	} else {
+		defer fh.Close() // nolint: errcheck
+		w.WriteHeader(200)
+		io.Copy(w, fh) // nolint: errcheck
+	}
+} // func (srv *Server) handleFavIco(w http.ResponseWriter, request *http.Request)
+
+func (srv *Server) handleStaticFile(w http.ResponseWriter, request *http.Request) {
+	// srv.log.Printf("[TRACE] Handle request for %s\n",
+	// 	request.URL.EscapedPath())
+
+	// Since we controll what static files the server has available, we
+	// can easily map MIME type to slice. Soon.
+
+	vars := mux.Vars(request)
+	filename := vars["file"]
+	path := filepath.Join("assets", "static", filename)
+
+	var mimeType string
+
+	srv.log.Printf("[TRACE] Delivering static file %s to client %s\n",
+		filename,
+		request.RemoteAddr)
+
+	var match []string
+
+	if match = common.SuffixPattern.FindStringSubmatch(filename); match == nil {
+		mimeType = "text/plain"
+	} else if mime, ok := srv.mimeTypes[match[1]]; ok {
+		mimeType = mime
+	} else {
+		srv.log.Printf("[ERROR] Did not find MIME type for %s\n", filename)
+	}
+
+	w.Header().Set("Content-Type", mimeType)
+
+	if common.Debug {
+		w.Header().Set("Cache-Control", "no-store, max-age=0")
+	} else {
+		w.Header().Set("Cache-Control", "max-age=7200")
+	}
+
+	var (
+		err error
+		fh  fs.File
+	)
+
+	if fh, err = assets.Open(path); err != nil {
+		msg := fmt.Sprintf("ERROR - cannot find file %s", path)
+		srv.sendErrorMessage(w, msg)
+	} else {
+		defer fh.Close() // nolint: errcheck
+		w.WriteHeader(200)
+		io.Copy(w, fh) // nolint: errcheck
+	}
+} // func (srv *Server) handleStaticFile(w http.ResponseWriter, request *http.Request)
+
+func (srv *Server) sendErrorMessage(w http.ResponseWriter, msg string) {
+	html := `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Internal Error</title>
+  </head>
+  <body>
+    <h1>Internal Error</h1>
+    <hr />
+    We are sorry to inform you an internal application error has occured:<br />
+    %s
+    <p>
+    Back to <a href="/index">Homepage</a>
+    <hr />
+    &copy; 2018 <a href="mailto:krylon@gmx.net">Benjamin Walkenhorst</a>
+  </body>
+</html>
+`
+
+	srv.log.Printf("[ERROR] %s\n", msg)
+
+	output := fmt.Sprintf(html, msg)
+	w.WriteHeader(500)
+	_, _ = w.Write([]byte(output)) // nolint: gosec
+} // func (srv *Server) sendErrorMessage(w http.ResponseWriter, msg string)
