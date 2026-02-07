d07eca58b82894fc26e706a4a49eea068f3d08b1
Benjamin Walkenhorst <krylon@posteo.de>
2026-02-07 15:12:13 +0100

    turn SQL script into shell script for convenience

--- dbstatus.sql
+++ dbstatus.sql
@@ -1,11 +1,16 @@
+#!/bin/sh
 -- /home/krylon/go/src/github.com/blicero/guangng/dbstatus.sql
--- Time-stamp: <2026-02-07 15:05:55 krylon>
+-- Time-stamp: <2026-02-07 15:12:01 krylon>
 -- created on 07. 02. 2026 by Benjamin Walkenhorst
 -- (c) 2026 Benjamin Walkenhorst
 -- Use at your own risk!
 
+sqlite3 ~/.guangng.d/guangng.db <<EOF
 SELECT COUNT(id) AS host_cnt FROM host;
 
 SELECT COUNT(id) AS svc_cnt FROM svc;
 
 SELECT COUNT(id) AS xfr_cnt FROM xfr WHERE status <> 0;
+
+EOF
+
