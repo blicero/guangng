75df5cf37c1a4771b30bc4449f542d51bbb7bdf5
Benjamin Walkenhorst <krylon@posteo.de>
2026-02-10 19:05:38 +0100

    refine query for scanned ports in dbstatus.sh

--- dbstatus.sh
+++ dbstatus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # /home/krylon/go/src/github.com/blicero/guangng/dbstatus.sql
-# Time-stamp: <2026-02-08 16:07:32 krylon>
+# Time-stamp: <2026-02-10 19:05:10 krylon>
 # created on 07. 02. 2026 by Benjamin Walkenhorst
 # (c) 2026 Benjamin Walkenhorst
 # Use at your own risk!
@@ -8,7 +8,7 @@
 sqlite3 ~/.guangng.d/guangng.db <<EOF
 SELECT COUNT(id) AS host_cnt FROM host;
 
-SELECT COUNT(id) AS svc_cnt FROM svc;
+SELECT COUNT(id) AS svc_cnt FROM svc WHERE COALESCE(response, '') <> '';
 
 SELECT COUNT(id) AS xfr_cnt FROM xfr WHERE status <> 0;
 
