80148106a359d84f144886604e810aab17fb1e37
Benjamin Walkenhorst <krylon@posteo.de>
2026-03-02 13:12:27 +0100

    common - bump version to 0.7.2

--- common/common.go
+++ common/common.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 23. 07. 2021 by Benjamin Walkenhorst
 // (c) 2021 Benjamin Walkenhorst
-// Time-stamp: <2026-02-27 18:57:06 krylon>
+// Time-stamp: <2026-03-02 13:12:19 krylon>
 
 // Package common contains definitions used throughout the application
 package common
@@ -36,7 +36,7 @@ import (
 // application.
 const (
 	AppName                  = "GuangNG"
-	Version                  = "0.7.1"
+	Version                  = "0.7.2"
 	Debug                    = true
 	DefaultPort              = 6942
 	TimestampFormatMinute    = "2006-01-02 15:04"
